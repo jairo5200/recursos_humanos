377999f97d95ede8f4d6745848236a3fbc1284e6
jairo5200 <jairo5200@gmail.com>
2025-01-20 18:47:49 -0500

    implementamos la excepcion not found

--- src/main/java/pt/rh/controlador/EmpleadoControlador.java
+++ src/main/java/pt/rh/controlador/EmpleadoControlador.java
@@ -3,7 +3,9 @@ package pt.rh.controlador;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pt.rh.excepcion.RecursoNoEncontradoExcepcion;
 import pt.rh.modelo.Empleado;
 import pt.rh.servicio.EmpleadoServicio;
 
@@ -19,25 +21,28 @@ public class EmpleadoControlador {
     @Autowired
     private EmpleadoServicio empleadoServicio;
 
-    @GetMapping("/empleado/")
+    @GetMapping("/empleados/")
     public List<Empleado> listarEmpleados(){
         List<Empleado> empleados = empleadoServicio.listarEmpleados();
         return empleados;
     }
 
-    @GetMapping("/empleado/{idEmpleado}")
-    public Empleado buscarEmpleadoPorId(@PathVariable Integer idEmpleado){
+    @GetMapping("/empleados/{idEmpleado}")
+    public ResponseEntity<Empleado> buscarEmpleadoPorId(@PathVariable Integer idEmpleado){
         Empleado empleado = empleadoServicio.buscarEmpleadoPorId(idEmpleado);
-        return empleado;
+        if (empleado == null) {
+            throw new RecursoNoEncontradoExcepcion("no se encontro el id: "+ idEmpleado);
+        }
+        return ResponseEntity.ok(empleado);
     }
 
-    @PostMapping("/empleado")
+    @PostMapping("/empleados/")
     public Empleado guardarEmpleado(@RequestBody Empleado empleado){
         Empleado elEmpleado = empleadoServicio.guardarEmpleado(empleado);
         return elEmpleado;
     }
 
-    @PutMapping("/empleado/{idEmpleado}")
+    @PutMapping("/empleados/{idEmpleado}")
     public Empleado actualizarEmpleado(@PathVariable Integer idEmpleado, @RequestBody Empleado empleado){
         Empleado elEmpleado = empleadoServicio.buscarEmpleadoPorId(idEmpleado);
         elEmpleado.setNombre(empleado.getNombre());
@@ -47,9 +52,9 @@ public class EmpleadoControlador {
         return elEmpleado;
     }
 
-    @DeleteMapping("empleado/{id}")
+    @DeleteMapping("empleados/{id}")
     public void eliminarEmpleado(@PathVariable Integer idEmpleado){
-        Empleado elEmpleado = empleadoServicio.buscarEmpleadoPorId(idEmpleado),
+        Empleado elEmpleado = empleadoServicio.buscarEmpleadoPorId(idEmpleado);
         empleadoServicio.eliminarEmpleado(elEmpleado);
     }
 }
--- src/main/java/pt/rh/modelo/Empleado.java
+++ src/main/java/pt/rh/modelo/Empleado.java
@@ -18,4 +18,37 @@ public class Empleado {
     String nombre;
     String departamento;
     Double sueldo;
+
+    public Integer getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public Double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
+
 }
