5351f69364e09d7c4be27570111bc7980a42e882
jairo5200 <jairo5200@gmail.com>
2025-01-20 14:02:48 -0500

    agregamos el controlador de empleados

---
+++ src/main/java/pt/rh/controlador/EmpleadoControlador.java
@@ -0,0 +1,22 @@
+package pt.rh.controlador;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pt.rh.servicio.EmpleadoServicio;
+
+@RestController
+//http://localhost:8080/rh-app/
+@RequestMapping("rh-app")
+@CrossOrigin(value = "http://localhost:3000")
+public class EmpleadoControlador {
+    private static final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
+
+    @Autowired
+    private EmpleadoServicio empleadoServicio;
+
+
+}
--- src/main/java/pt/rh/servicio/EmpleadoServicio.java
+++ src/main/java/pt/rh/servicio/EmpleadoServicio.java
@@ -1,27 +1,36 @@
 package pt.rh.servicio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pt.rh.modelo.Empleado;
+import pt.rh.repositorio.EmpleadoRepositorio;
 
 import java.util.List;
 
+@Service
 public class EmpleadoServicio implements IEmpleadoServicio {
+
+    @Autowired
+    private EmpleadoRepositorio empleadoRepositorio;
+
     @Override
     public List<Empleado> listarEmpleados() {
-        return null;
+        return empleadoRepositorio.findAll();
     }
 
     @Override
     public Empleado buscarEmpleadoPorId(Integer idEmpleado) {
-        return null;
+        Empleado elEmpleado = empleadoRepositorio.findById(idEmpleado).orElse(null);
+        return elEmpleado;
     }
 
     @Override
     public Empleado guardarEmpleado(Empleado elEmpleado) {
-        return null;
+        return  empleadoRepositorio.save(elEmpleado);
     }
 
     @Override
     public void eliminarEmpleado(Empleado elEmpleado) {
-
+        empleadoRepositorio.delete(elEmpleado);
     }
 }
